894d79271341be8c5bf00e674ccec79c167bbc1f
Rene Ramos <rene.ramos@onephase.mx>
2018-10-04 20:08:45 -0500

    Tarea Octubre 4

--- JS/main.js
+++ JS/main.js
@@ -1,25 +1,105 @@
-for(var i = 0; i < 21; i++){
-  console.log(i);
-}
+// for(var i = 0; i < 21; i++){
+//   console.log(i);
+// }
 
-var edad = 18;
+// var edad = 18;
 
-function isLegal(edad){
-  if(edad >= 18){
-    console.log('legal');
-  }else{
-    console.log('ilegal');
-  }
-}
+// function isLegal(edad){
+//   if(edad >= 18){
+//     console.log('legal');
+//   }else{
+//     console.log('ilegal');
+//   }
+// }
 
-isLegal(edad);
+// isLegal(edad);
 
-var base;
+// var base;
 
-function ac() {
-  base = prompt("Ingrese un numero:");
-  base = base * base;
-  console.log(base);
-}
+// function ac() {
+//   base = prompt("Ingrese un numero:");
+//   base = base * base;
+//   console.log(base);
+// }
 
-ac();
+// ac();
+
+
+// var numero = 1;
+// var nombre = "René";
+// var isAlive = true;
+// var lista = [1,2,3];
+// var persona = {
+//   nombre = '',
+//   edad: null,
+//   gender = '',
+//   alive = true;
+// }
+
+// function weekDay(){
+//   var numDay = parseInt(prompt('Ingrese un numero del 1 al 7', 'Numero'));
+//   switch (numDay) {
+//     case 1:
+//       console.log('Lunes');
+//       break;
+//     case 2: 
+//       console.log('Martes');
+//       break;
+//     case 3: 
+//       console.log('Miercoles');
+//       break;
+//     case 4: 
+//       console.log('Jueves');
+//       break;
+//     case 5: 
+//       console.log('Viernes');
+//       break;
+//     case 6: 
+//       console.log('Sabado');
+//       break;
+//     case 7: 
+//       console.log('Domingo');
+//       break;
+//     default : 
+//       console.log('No tecleaste un numero valido');
+//       weekDay();
+//   }
+// }
+// weekDay();
+
+// function zeroToHundred () {
+//   var i =0;
+//   for(i = 0; i < 101; i++){
+//     if(i == 10 || i == 20 || i == 30 || i == 40 || i == 50 || i == 60 || i == 70 || i == 80 || i == 90 || i == 100){
+//       console.log(i);
+//     }
+//   }
+// }
+
+// zeroToHundred();
+
+// function odd(){
+//   var i = 0;
+//   for(i = 0; i < 31; i++){
+//     if((i % 2) !== 0){
+//       console.log(i);
+//     }
+//   }
+// }
+
+// odd();
+
+// var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+// function oddOrNot(){
+//   var i = 0;
+//   for(i = 0; i <= numeros.length; i++){
+//     if(numeros[i] == 0){
+//       console.log('Es cero');
+//     }else if((numeros[i] % 2) == 0){
+//       console.log(numeros[i] + ' es par');
+//     }else{
+//       console.log(numeros[i] + ' es impar');
+//     }
+//   }
+// }
+// oddOrNot();
